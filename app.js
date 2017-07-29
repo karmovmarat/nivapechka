c8f32efe4e3f106864b6d1647bd39cb636db7c19
karmovmarat <karmov.23.javas@mail.com>
2017-07-29 09:14:10 -0700

    anyRequest.getconfig correct

--- app.js
+++ app.js
@@ -31,36 +31,37 @@ app.use(logger('dev'));
 
 // section settings param TAIMERS INTERVAL ( myInterval: time - msec, and f - callback)
 app.use(function(req, res, next) {
-  'use strict';
-  //11* Загрузка с локального диска базовой конфигурации  *****
-  // И отправка запроса на удаленный сервер 
-  // Для получения основной конфигурации расписания
-   if (!configSchedule) {
-      configSchedule = JSON.parse(fs.readFileSync(urlPathToConfig));
-      //     myRequest = anyRequest(configSchedule); // first load
-      console.log(configSchedule.timeIntervalMsec + "  первая загрузка");
-      // myInterval.setConfig(configSchedule.timeIntervalMsec, myRequest.send);
-      myRequest.setConfig(configSchedule);
-      myRequest.send();
-      console.log('myRequest.getConfig()');
-      console.log(myRequest.getConfig()); 
-  }
-  //22* Загрузка основной конфигурации в модуль ИНТЕРВАЛ ****
-  // и в модуль ЗАПРОС, Установка счетчика для обновления 
-  //  основной конфигурации через каждые N раз
-  else if (configSchedule.firstLoad || (countRepeatLoadSheduleEvery > 10)) {
-    countRepeatLoadSheduleEvery = 0;
-    configSchedule = myRequest.getConfig();
-    console.log(configSchedule.timeIntervalMsec + "  вторая загрузка");
-    myInterval.setConfig(configSchedule.timeIntervalMsec, myRequest.send);
-    myRequest.setConfig(configSchedule);
-    //configSchedule.firstLoad = false;
-    console.log(configSchedule.firstLoad);
-  }
-  //**********************
-  countRepeatLoadSheduleEvery += 1;
-  console.log(' countRepeatLoadSheduleEvery  ' + countRepeatLoadSheduleEvery);
-  next();
+    'use strict';
+    //11* Загрузка с локального диска базовой конфигурации  *****
+    // И отправка запроса на удаленный сервер 
+    // Для получения основной конфигурации расписания
+    if (!configSchedule) {
+        
+        configSchedule = JSON.parse(fs.readFileSync(urlPathToConfig));
+        //     myRequest = anyRequest(configSchedule); // first load
+        console.log(configSchedule.timeIntervalMsec + "  первая загрузка");
+        // myInterval.setConfig(configSchedule.timeIntervalMsec, myRequest.send);
+        myRequest.setConfig(configSchedule);
+        myRequest.send();
+        console.log('myRequest.getConfig()');
+        console.log(myRequest.getConfig());
+    }
+    //22* Загрузка основной конфигурации в модуль ИНТЕРВАЛ ****
+    // и в модуль ЗАПРОС, Установка счетчика для обновления 
+    //  основной конфигурации через каждые N раз
+    else if (configSchedule.firstLoad || (countRepeatLoadSheduleEvery > 10)) {
+        countRepeatLoadSheduleEvery = 0;
+        configSchedule = myRequest.getConfig();
+        console.log(configSchedule.timeIntervalMsec + "  вторая загрузка");
+        myInterval.setConfig(configSchedule.timeIntervalMsec, myRequest.send);
+        myRequest.setConfig(configSchedule);
+        //configSchedule.firstLoad = false;
+        console.log(configSchedule.firstLoad);
+    }
+    //**********************
+    countRepeatLoadSheduleEvery += 1;
+    console.log(' countRepeatLoadSheduleEvery  ' + countRepeatLoadSheduleEvery);
+    next();
 });
 //******************* end section settings param TAIMERS INTERVAL
 
@@ -68,10 +69,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true, // true = .sass and false = .scss
+    sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 //************************************************
@@ -93,21 +94,20 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
-
