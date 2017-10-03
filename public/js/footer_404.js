c19397bf7f602ee477f1042812b3034f3a04858d
karmovmarat <karmov.23.javas@mail.com>
2017-10-03 00:49:45 -0700

    nivapechka first commit HRK

---
+++ public/js/footer_404.js
@@ -0,0 +1,7 @@
+
+document.write(" <p>© 2012 - 2017 г. Сайт <a target=\"_blank\" href=\"http://nivapechka.com\">nivapechka.com</a>. По всем вопросам пишите по адресу nivapechka@gmail.com  </p>\
+<a target=\"_blank\" href=\"../../wordpress/legal_ru.html\">Юридическая информация: </a>\
+Копирование материалов сайта возможно только с письменного согласия \
+автора. Полное или частичное использование материалов сайта в сети \
+Интернет разрешается при условии размещения прямой активной ссылки на сайт  <a href=\"http://nivapechka.com\">nivapechka.com</a>\
+");
